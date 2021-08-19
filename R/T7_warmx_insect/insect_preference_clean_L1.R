022ea3ae9d7064f764d2482254ed864f933c1b39
moriahy <yomoriah@gmail.com>
2021-08-19 15:59:14 -0400

    creating insect preference trial clean script

---
+++ R/T7_warmx_insect/insect_preference_clean_L1.R
@@ -0,0 +1,22 @@
+# TITLE:          REX: Insect preference trial clean-up
+# AUTHORS:        Moriah Young
+# COLLABORATORS:  
+# DATA INPUT:     Data imported as csv files from shared REX Google drive T7_warmx_insect L0 folder
+# DATA OUTPUT:    Clean L1 data uploaded to T7_warmx_insect L1 folder
+# PROJECT:        REX
+# DATE:           August 2021
+
+# Clear all existing data
+rm(list=ls())
+
+# Load packages
+library(tidyverse)
+
+# Set working directory from .Renviron
+dir <- Sys.getenv("DATA_DIR")
+list.files(dir)
+
+# Read in data
+insects <- read.csv(file.path(dir, "T7_warmx_insect/L0/2021/T7_warmx_insect_preference_L0.csv"))
+
+View(insects)
