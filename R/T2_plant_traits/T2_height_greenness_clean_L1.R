da12fdc2d63dcab865503dde860e4f5b9cfbdcc6
moriahy <yomoriah@gmail.com>
2022-08-04 20:49:01 -0400

    started cleaning script for T7 warmx phenology data

--- R/T2_plant_traits/T2_height_greenness_clean_L1.R
+++ R/T2_plant_traits/T2_height_greenness_clean_L1.R
@@ -1,10 +1,10 @@
 # TITLE:          REX: T2 plots height and greenness measurements on wheat
-# AUTHORS:        Moriah Young
-# COLLABORATORS:  Lisa Leonard
+# AUTHORS:        Moriah Young, Lisa Leonard
+# COLLABORATORS:  
 # DATA INPUT:     Data imported as csv files from shared REX Google drive L0 folder
 # DATA OUTPUT:    Clean L1 data uploaded to L1 folder
 # PROJECT:        REX
-# DATE:           JuLY 2022
+# DATE:           July 2022
 
 # Clear all existing data
 rm(list=ls())
@@ -18,4 +18,5 @@ list.files(dir)
 
 # Read in data
 wheat <- read.csv(file.path(dir, "T2_height_greenness_2022_LO.csv"))
+# wheat <- read.csv("/Users/moriahyoung/Downloads/T2_height_greenness_2022_L0 - Sheet1.csv")
 
