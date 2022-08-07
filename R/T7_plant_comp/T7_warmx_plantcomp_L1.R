dd6be6f027080ae522b7835e7fde14e5a0a6c279
moriahy <yomoriah@gmail.com>
2022-08-07 19:57:19 -0400

    adding 2021 plant comp data to cleaning scripts

--- R/T7_plant_comp/T7_warmx_plantcomp_L1.R
+++ R/T7_plant_comp/T7_warmx_plantcomp_L1.R
@@ -18,11 +18,18 @@ dir <- Sys.getenv("DATA_DIR")
 list.files(dir)
 
 # Read in data
+plantcomp21 <- read.csv(file.path(dir, "T7_plant_comp/L0/T7_plantcomp_2021_L0.csv"))
 plantcomp22 <- read.csv(file.path(dir, "T7_plant_comp/L0/T7_warmx_plantcomp_2022_L0.csv"))
 meta <- read.csv(file.path(dir, "REX_warmx_metadata.csv"))
 taxon <- read.csv(file.path(dir, "REX_warmx_taxon.csv"))
 
-plantcomp22 <- plantcomp22[-c(997:1006),]
+plantcomp22 <- plantcomp22[-c(997:1006),] # delete rows with NAs
+
+# 2021 data
+plantcomp21 <- plantcomp21[,-c(1, 3, 5, 10, 11)] # delete unnecessary columns
+names(plantcomp21)[names(plantcomp21)=="New_Footprint"] <- "Footprint_Location"
+names(plantcomp21)[names(plantcomp21)=="Quad"] <- "Subplot_Location"
+names(plantcomp21)[names(plantcomp21)=="Species"] <- "Code"
 
 # 2022 data
 plantcomp22 <- plantcomp22[,-c(1, 3, 9, 10)] # delete unnecessary columns
@@ -30,27 +37,31 @@ names(plantcomp22)[names(plantcomp22)=="Footprint"] <- "Footprint_Location"
 names(plantcomp22)[names(plantcomp22)=="Quad"] <- "Subplot_Location"
 names(plantcomp22)[names(plantcomp22)=="Species"] <- "Code"
 
-plantcomp22$Date <- mdy(plantcomp22$Date) # change date to %m/%d/%Y format
-plantcomp22[["Date"]] <- as.Date(plantcomp22[["Date"]],format="%m/%d/%Y")
+# merge 2021 and 2022 data together
+plantcomp <-  full_join(plantcomp21, plantcomp22)
+
+plantcomp$Date <- mdy(plantcomp$Date) # change date to %m/%d/%Y format
+plantcomp[["Date"]] <- as.Date(plantcomp[["Date"]],format="%m/%d/%Y")
 
 # check that there aren't any misspellings
-unique(sort(plantcomp22[["Code"]])) # check that there aren't any misspellings
-unique(sort(plantcomp22[["Date"]])) # check that there are no weird dates
+unique(sort(plantcomp[["Code"]])) # check that there aren't any misspellings
+unique(sort(plantcomp[["Date"]])) # check that there are no weird dates
 
 # change species names
-plantcomp22$Code[plantcomp22$Code == "Brin "] <- "Brin"
+plantcomp$Code[plantcomp$Code == "Brin "] <- "Brin"
+plantcomp$Code[plantcomp$Code == "Hisp "] <- "Hisp"
 
 # combine meta data with plant comp dataframe
-plantcomp22 <- merge(plantcomp22, meta, by=(c("Rep", "Footprint_Location", "Subplot_Location")))
+plantcomp1 <- merge(plantcomp, meta, by=(c("Rep", "Footprint_Location", "Subplot_Location")))
 
-plantcomp22 <- plantcomp22[,-1] # delete unnecessary columns
+plantcomp1 <- plantcomp1[,-1] # delete unnecessary columns
 
-plantcomp22[["Date"]] <- as.Date(plantcomp22[["Date"]],format="%m/%d/%Y")
+plantcomp1[["Date"]] <- as.Date(plantcomp1[["Date"]],format="%m/%d/%Y")
 
 # make a column for Julian date
-plantcomp22$Julian <- format(plantcomp22$Date, "%j")
+plantcomp1$Julian <- format(plantcomp1$Date, "%j")
 
-plantcomp22 <- plantcomp22[,c(6, 7, 1, 2, 8, 9, 11, 10, 4, 5, 3, 12)]
+plantcomp1 <- plantcomp1[,c(6, 7, 1, 2, 8, 9, 11, 10, 4, 5, 3, 12)]
 
 # upload L1 data
 write.csv(plantcomp22, file.path(dir,"T7_plant_comp/L1/T7_warmx_plantcomp_L1.csv"), row.names=F)
