0120d149e3d9e7a9902d300c6747fe19c7871e8f
dobsonk2 <65179695+dobsonk2@users.noreply.github.com>
2023-01-09 11:23:00 -0500

    script to clean 2022 ANPP data

---
+++ R/T7_ANPP/T7_warmx_ANPP_2022_clean_L1.R
@@ -0,0 +1,49 @@
+# TITLE:          REX: Phoebe's plots ANPP
+# AUTHORS:        Kara Dobson
+# COLLABORATORS:  Phoebe Zarnetske, Moriah Young, Mark Hammond, Jordan Zapata
+# DATA INPUT:     Data imported as csv files from shared REX Google drive T7_ANPP L0 folder
+# DATA OUTPUT:    Clean L1 data uploaded to T7_ANPP L1 folder
+# PROJECT:        REX
+# DATE:           Jan 2023
+
+# Clear all existing data
+rm(list=ls())
+
+# Load packages
+library(tidyverse)
+
+# Set working directory from .Renviron
+dir <- Sys.getenv("DATA_DIR")
+list.files(dir)
+
+# Read in data
+anpp_data <- read.csv(file.path(dir, "T7_ANPP/L0/2022 All footprints/REX_ANPP_2022_biomass_final.csv"))
+meta <- read.csv(file.path(dir, "REX_warmx_metadata.csv"))
+taxon <- read.csv(file.path(dir, "REX_warmx_taxon.csv"))
+
+# Making meta-data file match the format of the ANPP data
+meta$Treatment <- 7
+meta$Field_Loc_Code <- paste0(meta$Treatment, "_", meta$Rep, "_", meta$Footprint_Location, "_", meta$Subplot_Location)
+
+# removing unneeded columns in ANPP
+anpp_data = subset(anpp_data, select = -c(Date_of_Harvest,Field_Treatment_Number,Field_Rep,Footprint_Number,Subplot_Letter,
+                                          Unique_Field_Location_Code,Type_of_Weighing._Direct_or_Indirect,Dried_Bag.mass_g,
+                                          Type_of_Bag,Sum_Bag_plus_Dried_Plant_Biomass_gram,Notes,data_entry_ID,Mark_needs_to_do_further_proofing))
+
+# merging meta data with ANPP data
+anpp <- left_join(anpp_data, meta, by = c("Field_Loc_Code"))
+
+# subset out Phoebe's plots and the irrigated control
+anpp <- anpp %>%
+  filter(Footprint_Owner == "PZ" | Subplot_Descriptions == "irrigated_control")
+
+# removing unneeded columns
+anpp = subset(anpp, select = -c(Footprint_Owner, Replicate, Footprint, Subplot,Unique_ID))
+
+# making all species capitalized
+anpp$Species_Code = toupper(anpp$Species_Code)
+
+# adding together live and dead clover measurements
+
+
+
