73b21b8757d8a12d0e7dd5cfafd607266c42bf84
moriahy <yomoriah@gmail.com>
2024-06-19 09:23:02 -0400

    adding soil gwc

--- R/T7_soil/T7_warmx_soil_moisture_clean_L1.R
+++ R/T7_soil/T7_warmx_soil_moisture_clean_L1.R
@@ -1,10 +1,10 @@
-# TITLE:          REX: T7 plots soil moisture
+# TITLE:          REX: T7 warmx plots soil moisture (GWC)
 # AUTHORS:        Moriah Young
 # COLLABORATORS:  
 # DATA INPUT:     Data imported as csv files from shared REX Google drive T7_soil L0 folder
 # DATA OUTPUT:    Clean L1 data uploaded to T7_soil L1 folder
 # PROJECT:        REX
-# DATE:           Jan 2023
+# DATE:           Jan 2023; updated June 2024
 
 # Clear all existing data
 rm(list=ls())
@@ -17,14 +17,17 @@ dir <- Sys.getenv("DATA_DIR")
 list.files(dir)
 
 # Read in data
-soil <- read.csv(file.path(dir, "soil/L0//REX_Y2_Microbial soils_Soil moisture.csv"))
+soil22 <- read.csv(file.path(dir, "soil/L0/REX_Y2_Microbial soils_Soil moisture.csv"))
+predrought23 <- read.csv(file.path(dir, "soil/L0/T7_warmx_predrought_soil_moisture_2023.csv"))
+soil <- read.csv(file.path(dir, "soil/L0/REX_Microbial_Soil_GWC.csv"))
+microbe_meta <- read.csv(file.path(dir, "REX_Microbial_sampling_IDs_complete.csv"))
 meta <- read.csv(file.path(dir, "REX_T7_metadata.csv"))
 
 # make plot ID columns the same name
-names(soil)[names(soil)=="Plot.ID"] <- "Unique_ID"
+names(soil22)[names(soil22)=="Plot.ID"] <- "Unique_ID"
 
 # merge both data frames by "Unique_ID"
-soil_1 <- left_join(soil, meta, by = "Unique_ID")
+soil_1 <- left_join(soil22, meta, by = "Unique_ID")
 
 # Exclude rows where Footprint_Treatment_full is NA (non T7 footprints):
 soil_2 <- soil_1 %>% filter(!is.na(Footprint_Treatment_full))
@@ -36,10 +39,12 @@ unique(soil_2$FP_treatment)
         
 # removing unneeded columns
 soil_3 <- subset(soil_2, select = -c(bag.wt, bag...wet.soil, bag...dry.soil, wet.soil..g., dry.soil..g., to_check, Notes, 
-                                    who.has.fresh.soil.))
+                                    who.has.fresh.soil., Sample_Num))
 
 # rename "comments" column to "time_of_sampling"
 colnames(soil_3)[colnames(soil_3) == "comments"] = "time_of_sampling"
+colnames(soil_3)[colnames(soil_3) == "grav_soil_moisture"] = "gwc"
+colnames(soil_3)[colnames(soil_3) == "Sample.ID"] = "Sample_ID"
 
 # code below is to get a dataframe for just T7 warmx plots
 # select for irrigated control, OTCs under rainout shelters, and OTC control footprints
@@ -47,9 +52,50 @@ warmx_1 <- soil_3 %>% filter(FP_treatment %in% c("IR", "OR", "OC"))
 
 # we want to filter out fungicide, nematicide, and sorghum subplot manipulations
 warmx_2 <- warmx_1 %>% filter(!Subplot_Description %in% c("Fungicide", "Nematicide", "Sorghum"))
+warmx_2 <- warmx_2[-c(1,2),]
 
 # upload L1 data
 write.csv(soil_3, file.path(dir,"soil/L1/T7_soil_moisture_2022_L1.csv"), row.names=F) # all T7s
 
 write.csv(warmx_2, file.path(dir,"soil/L1/T7_warmx_soil_moisture_2022_L1.csv"), row.names=F) # just warmx plots
 
+###############################################################################
+# 2021-2023 
+
+# delete columns
+microbe_meta <- microbe_meta[,1:14]
+
+soil_join <- full_join(soil, microbe_meta, by = c("Sample_ID", "Treatment", "Replicate"))
+
+# removing unneeded columns
+soil_join <- soil_join %>% select (-Order, -Subplot, -comments, -Datetime_UTC)
+
+# rename "Sampling.Time.Point" column to "time_of_sampling"
+colnames(soil_join)[colnames(soil_join) == "Sampling.Time.Point"] = "time_of_sampling"
+
+colnames(soil_join)[colnames(soil_join) == "Plot_ID"] = "Unique_ID"
+
+# reorder columns in dataframe
+soil_join <- soil_join[, c("date", "Sample_ID", "Unique_ID", "Year", "gwc", "Treatment", "Replicate", 
+                           "Footprint", "FP_location", "FP_treatment", "Subplot_location", "Subplot_treatment",
+                           "Footprint_Treatment_full", "flood_compromised", "time_of_sampling")]
+
+# code below is to get a dataframe for just T7 warmx plots
+# select for irrigated control, OTCs under rainout shelters, and OTC control footprints
+warmx_3 <- soil_join %>% filter(Treatment %in% c("T7"))
+warmx_4 <- warmx_3 %>% filter(FP_treatment %in% c("IR", "OR", "OC"))
+warmx_5 <- warmx_4 %>% filter(!Subplot_treatment %in% c("F","S","N"))
+warmx_6 <- na.omit(warmx_5)
+
+write.csv(warmx_6, file.path(dir,"soil/L1/T7_warmx_soil_moisture_L1.csv"), row.names=F) # just warmx plots
+
+###############################################################################
+# pre drought 2023
+
+# calculate gwc
+predrought_gwc <- predrought23 %>% 
+        mutate(gwc = (wet.soil...tin - dry.soil...tin) - tin_weight)
+# hmmm this is not right
+
+        
+        
